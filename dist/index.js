bd517a03499c13aaa600cc9a8e0b569b239bb56a
Muhammed Ahmed <ma3574@users.noreply.github.com>
2020-09-04 19:10:54 +0100

    New changes

--- dist/index.js
+++ dist/index.js
@@ -1393,7 +1393,7 @@ async function run() {
     const hosts = core.getInput('hosts');
     core.info(`Add to hosts: \n${hosts}`);
     exec.exec(`sudo echo '${hosts}' >> /etc/hosts`)
-    exec.exec(`sudo echo '122.0.0.1 nano.com' >> /etc/hosts`)
+    // exec.exec(`sudo echo '122.0.0.1 nano.com' >> /etc/hosts`)
     exec.exec(`sudo -- sh -c 'echo "127.0.0.1  mongo.private" >> /etc/hosts'`)
     // exec.exec(`echo`, ['hosts file content:'])
     exec.exec('cat', ['/etc/hosts'])
