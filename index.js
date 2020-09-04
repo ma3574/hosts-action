24d29d0b77180a1aa5915dca2ae84e6d8489631c
Muhammed Ahmed <ma3574@users.noreply.github.com>
2020-09-04 16:36:29 +0100

    Add writing to hosts file

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ async function run() {
     const hosts = core.getInput('hosts');
     core.info(`Add to hosts: \n${hosts}`);
     exec.exec(`echo ${hosts} >> /etc/hosts`)
+    exec.exec(`echo '122.0.0.1 nano.com' >> /etc/hosts`)
     exec.exec(`echo 'hosts file content:' && cat /etc/hosts`)
     // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     // core.setOutput('time', new Date().toTimeString());
