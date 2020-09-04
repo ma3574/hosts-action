ed5b0e0d75cd92d085bab5986d8a8f660c09bd41
Muhammed Ahmed <ma3574@users.noreply.github.com>
2020-09-04 16:34:29 +0100

    Add writing to hosts file

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const hosts = core.getInput('hosts');
-    core.info(`Hosts data is: \n${hosts}`);
-
+    core.info(`Add to hosts: \n${hosts}`);
+    exec.exec(`echo ${hosts} >> /etc/hosts`)
+    exec.exec(`echo 'hosts file content:' && cat /etc/hosts`)
     // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    // await wait(parseInt(ms));
-    // core.info((new Date()).toTimeString());
     // core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
